056557b3af1ed5d36fe3d4a85af5329e0f929b63
Cyber1551 <develop@brandonlacy.com>
2026-05-11 14:20:32 -0500

    ESLint: add react plugin and consistent-type-imports rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import react from 'eslint-plugin-react'
 import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
@@ -14,14 +15,26 @@ export default tseslint.config(
       globals: globals.browser,
     },
     plugins: {
+      react,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
+    },
+    settings: {
+      // TODO: https://github.com/jsx-eslint/eslint-plugin-react/issues/3977
+      // eslint-plugin-react's `detect` is incompatible with ESLint 10's new rule-context API.
+      react: { version: '19' },
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': [
         'warn',
         { allowConstantExport: true },
+      ],
+      'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
+      'react/display-name': 'warn',
+      '@typescript-eslint/consistent-type-imports': [
+        'warn',
+        { prefer: 'type-imports', fixStyle: 'inline-type-imports' },
       ],
     },
   },
